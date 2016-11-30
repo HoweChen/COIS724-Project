c038e14c6d336710c167e7de64f22b3d0d0fdd36
SkyFather <cyh278889789@gmail.com>
2016-11-30 10:43:57 +0800

    update StayPoints folder and cluseter.py

--- stay_points.py
+++ stay_points.py
@@ -2,92 +2,97 @@ import glob
 import time
 import datetime
 from geopy.distance import vincenty
+import sys
 
-stay_points_file = open("stay_points.txt", "w")
+# user_id = sys.argv[1]
+# user_id = str(001)
+for user_id in range(0,10):
+    stay_points_file = open("./StayPoints/00" + str(user_id) + ".txt", "w")
 
-data = glob.glob('Data/002/Trajectory/*.*')
+    data = glob.glob('Data/00' + str(user_id) + '/Trajectory/*.*')
 
-days = [list(open(d, "r")) for d in data]
-days = [d[6:] for d in days]
-total_stay_points = 0  # stay points for this user
-for i in range(len(days)):
+    days = [list(open(d, "r")) for d in data]
+    days = [d[6:] for d in days]
+    total_stay_points = 0  # stay points for this user
+    for i in range(len(days)):
 
-    prev_second = 0
-    curr_second = 0
+        prev_second = 0
+        curr_second = 0
 
-    total_seconds = 0
+        total_seconds = 0
 
-    start_lat = 0
-    start_long = 0
+        start_lat = 0
+        start_long = 0
 
-    curr_lat = 0
-    curr_long = 0
+        curr_lat = 0
+        curr_long = 0
 
-    total_lat = 0
-    total_long = 0
+        total_lat = 0
+        total_long = 0
 
-    total_distance = 0
-    counter = 0
+        total_distance = 0
+        counter = 0
 
-    for j in range(len(days[i])):
-        days[i][j] = days[i][j].strip('\r\n').split(',')
-        x = days[i][j][-1]
-        x = time.strptime(x, '%H:%M:%S')
-        curr_second = datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
+        for j in range(len(days[i])):
+            days[i][j] = days[i][j].strip('\r\n').split(',')
+            x = days[i][j][-1]
+            x = time.strptime(x, '%H:%M:%S')
+            curr_second = datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
 
-        curr_lat = days[i][j][0]
-        curr_long = days[i][j][1]
+            curr_lat = days[i][j][0]
+            curr_long = days[i][j][1]
 
-        if j == 0:
+            if j == 0:
+                prev_second = curr_second
+                start_lat = curr_lat
+                start_long = curr_long
+
+            total_seconds += int(curr_second) - int(prev_second)
+            # print "The time difference is %s and j is %s" % (str(int(curr_second) - int(prev_second)), str(j))
+
             prev_second = curr_second
-            start_lat = curr_lat
-            start_long = curr_long
 
-        total_seconds += int(curr_second) - int(prev_second)
+            curr_pos = (curr_lat, curr_long)
+            start_pos = (start_lat, start_long)
 
-        prev_second = curr_second
+            total_lat += float(curr_lat)
+            total_long += float(curr_long)
+            counter += 1
 
-        curr_pos = (curr_lat, curr_long)
-        start_pos = (start_lat, start_long)
+            total_distance = vincenty(start_pos, curr_pos).meters
 
-        total_lat += float(curr_lat)
-        total_long += float(curr_long)
-        counter += 1
+            if total_seconds >= 1200 and total_distance <= 200:
 
-        total_distance = vincenty(start_pos, curr_pos).meters
+                print "stay point"
+                print "the time is %s %s" % (
+                    str(total_seconds / 60.), "minutes" if (total_seconds / 60.) > 60 else "minutes")
+                print "the distance is %s metres" % total_distance
 
-        if total_seconds >= 1200 and total_distance <= 200:
+                stay_p_lat = total_lat / counter
+                stay_p_long = total_long / counter
 
-            print "stay point"
-            print "the time is %s %s" % (
-                str(total_seconds / 60.), "hours" if (total_seconds / 60.) > 60 else "minutes")
-            print "the distance is %s metres" % total_distance
+                start_lat = curr_lat
+                start_long = curr_long
 
-            stay_p_lat = total_lat / counter
-            stay_p_long = total_long / counter
+                total_stay_points += 1
 
-            start_lat = curr_lat
-            start_long = curr_long
+                total_seconds = 0
+                total_lat = 0
+                total_long = 0
+                counter = 0
 
-            total_stay_points += 1
+                # save the stay point to a file
+                stay_points_file.write("%s, %s\n" % (str(stay_p_lat), str(stay_p_long)))
 
-            total_seconds = 0
-            total_lat = 0
-            total_long = 0
-            counter = 0
+            elif total_seconds >= 1200 and total_distance > 200:
+                total_seconds = 0
+                start_lat = curr_lat
+                start_long = curr_long
 
-            # save the stay point to a file
-            stay_points_file.write("%s, %s\n" % (str(stay_p_lat), str(stay_p_long)))
+                total_lat = 0
+                total_long = 0
+                counter = 0
 
-        elif total_seconds >= 1200 and total_distance > 200:
-            total_seconds = 0
-            start_lat = curr_lat
-            start_long = curr_long
+        print "There are %s stay points for this date" % str(total_stay_points)
 
-            total_lat = 0
-            total_long = 0
-            counter = 0
-
-    print "There are %s stay points for this date" % str(total_stay_points)
-
-    # remove this break if you want to run all the dates for the user
+        # remove this break if you want to run all the dates for the user
