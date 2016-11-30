cb608c8c4ebe082f00fd63988fb538cf2c8c0004
SkyFather <cyh278889789@gmail.com>
2016-11-30 16:48:27 +0800

    update main program

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World");
+import os
+
+print "Calculating the stay points..."
+os.system('python ./stay_points.py')
+print "Calculating the clusters..."
+os.system('python ./cluster.py')
+print "Job done."
